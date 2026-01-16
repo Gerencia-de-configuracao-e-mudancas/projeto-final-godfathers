09ed7b923a70e0586d96284b8637b6a5a17f8cf2
Ruan Vituriano <ruanviturianoc@gmail.com>
2026-01-16 12:08:33 -0300

    Adicionando logica de xp

--- script/index.js
+++ script/index.js
@@ -3,18 +3,20 @@ personagem = {
     xpAtual: 0,
     xpNecessario: 100,
     level: 0,
+    money: 0,
     itens: []
 }
 
 
 function uparPersonagem(quantidade) {
-    personagem.xpAtual += quantidade;
-    
+    personagem.xpAtual += quantidade
+    personagem.money += 100
     while (personagem.xpAtual >= personagem.xpNecessario) {
         personagem.xpAtual -= personagem.xpNecessario;
         personagem.level += 1;
-        personagem.xpNecessario *= 2;
+        personagem.xpNecessario *= 2
         alert("Level up!")
+        console.log(`level UP!`, personagem)
   }
   console.log('XP atual:', personagem.xpAtual, 'Level:', personagem.level, 'XP necessário:', personagem.xpNecessario);
 }
